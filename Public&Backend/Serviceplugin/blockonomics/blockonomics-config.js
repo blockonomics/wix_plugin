762c72faf2def370ea8126e9b10dca6e90c1fe5c
Ash <91294460+ashthecoder05@users.noreply.github.com>
2024-12-13 15:01:31 +0000

    Update blockonomics-config.js

--- Public&Backend/Serviceplugin/blockonomics/blockonomics-config.js
+++ Public&Backend/Serviceplugin/blockonomics/blockonomics-config.js
@@ -4,7 +4,7 @@ import * as paymentProvider from 'interfaces-psp-v1-payment-service-provider';
 export function getConfig() {
     // Customer Creates a blockonomics store with zpub, address and callback url.
         // where to get callback url?
-        // Get wix store address (https://aishwaryaadyanthay.wixsite.com/my-site)
+        // Get wix store address 
         // Our url extension (_functions/blockonomicscallback?secret=123353)
 
     return {
